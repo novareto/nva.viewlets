650e9669b2f6df5c26dadfc74f2f5216057933a1
Lars Walther <meinkraeks@gmail.com>
2019-08-28 13:32:05 +0200

    Neuer View für leichte Sprache

--- src/nva/viewlets/viewlets/leichte_sprache_viewlet.py
+++ src/nva/viewlets/viewlets/leichte_sprache_viewlet.py
@@ -6,16 +6,22 @@ from plone.app.layout.viewlets import ViewletBase
 class LeichteSpracheViewlet(ViewletBase):
 
     def update(self):
-        self.leichtesprache = self.get_leichtesprache()
+        self.button = self.get_button()
 
-    def get_leichtesprache(self):
-        if hasattr(self.context, 'leichtesprache'):
-            if self.context.leichtesprache:
-                return self.context.leichtesprache.output
+    def get_button(self):
+        button = {}
+        button['name'] = u'Leichte Sprache'
+        button['url'] = self.context.absolute_url() + '/leichte-sprache'
+        if self.request.URL.endswith('leichte-sprache'):
+            button['name'] = u'Standardsprache'
+            button['url'] = self.context.absolute_url()
+        return button
 
     def render(self):
         if hasattr(self.context, 'leichtesprache'):
             if self.context.leichtesprache:
                 return super(LeichteSpracheViewlet, self).render()
+            else:
+                return ""
         else:
             return ""
--- src/nva/viewlets/views/leichte_sprache.py
+++ src/nva/viewlets/views/leichte_sprache.py
@@ -3,15 +3,10 @@
 from nva.viewlets import _
 from Products.Five.browser import BrowserView
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
 class LeichteSprache(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('leichte-sprache.pt')
 
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def leichtesprache(self):
+        if hasattr(self.context, 'leichtesprache'):
+            if self.context.leichtesprache:
+                return self.context.leichtesprache.output
+        return u""
