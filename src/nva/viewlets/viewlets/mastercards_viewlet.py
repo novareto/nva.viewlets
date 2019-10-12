e929a0032e2ad83a1c1595eb62bf25c082e9b35e
Lars Walther <lwalther@novareto.de>
2019-10-12 22:07:56 +0200

    mastercard-viewlet

--- src/nva/viewlets/viewlets/mastercards_viewlet.py
+++ src/nva/viewlets/viewlets/mastercards_viewlet.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from plone import api as ploneapi
 
+hide_layouts = ['enhanced_cardview', 'enhanced_cardcolumns', 'zwei_spalten_view', 'zwei_spalten_context_view']
 
 class MastercardsViewlet(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
-
-    def get_message(self):
-        return u'My message'
+    def get_cards(self):
+        objview = ploneapi.content.get_view(name='enhanced_foldersummary', context=self.context, request=self.request)
+        cards = objview.getContextCards(self.context)
+        size = 3
+        if cards:
+            return [cards[i:i+size] for i in range(0, len(cards), size)]
+        return []
 
     def render(self):
-        return super(MastercardsViewlet, self).render()
+        if self.context.layout in hide_layouts:
+            return ""
+        if hasattr(self.context, 'cards'):
+            if self.context.cards:
+                return super(MastercardsViewlet, self).render()
+            else:
+                return ""
+        else:
+            return ""
