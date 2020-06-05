9df90ea045cda16fc93ac03d84c911603a108f28
Lars Walther <lwalther@novareto.de>
2020-06-05 10:26:22 +0200

    DocumentActions viewlet

--- src/nva/viewlets/viewlets/document_actions_viewlet.py
+++ src/nva/viewlets/viewlets/document_actions_viewlet.py
@@ -6,10 +6,25 @@ from plone.app.layout.viewlets import ViewletBase
 class DocumentActionsViewlet(ViewletBase):
 
     def update(self):
-        self.message = self.get_message()
+        import pdb;pdb.set_trace()
+        self.webcode = self.actions.append(self.get_webcode())
+        self.actions = []
+        self.actions.append(self.get_standard_actions())
 
-    def get_message(self):
-        return u'My message'
+    def get_standard_actions(self):
+        """
+        Return the standard plone actions
+        """
+        context = self.context
+        actions = context.portal_actions.listFilteredActionsFor(context)
+        document_actions = filtered.get('document_actions', [])
+        return document_actions
+
+    def getWebcode(self):
+        """Holt den Webcode fuer den Inhalt"""
+        if hasattr(self.context.aq_inner, 'webcode'):
+            return self.context.aq_inner.webcode
+        return ''
 
     def render(self):
         return super(DocumentActionsViewlet, self).render()
